125715c5e2693f30d29017c285c9ee40a5a24f64
acskii <andrew.sameh.06@gmail.com>
2025-01-07 19:00:27 +0200

    improved substrings

--- ruby_projects/substring.rb
+++ ruby_projects/substring.rb
@@ -1,36 +1,19 @@
 # acskii :p
 
-def substrings(original, subs)
+# Improved version
+def substrings(word, subs)
   subs.reduce(Hash.new) do |tally, sub|
-    original_a = original.downcase
-    p = 0
+    current_word = word.downcase.split(" ")
     
-    while p < original.length - 1
-      if sub[0] == original_a[p]
-        j = 1
-
-        while j <= sub.length-1
-          p += 1
-          
-          if sub[j] != original_a[p]
-            break
-          end
-
-          j += 1
-        end
-        
-        if j == sub.length
-          if tally.dig(sub)
-            tally[sub] += 1
-          else
-            tally[sub] = 1
-          end
+    current_word.each do |word|
+      if word.include? sub
+        if tally.dig(sub)
+          tally[sub] += 1
+        else
+          tally[sub] = 1
         end
       end
-      
-      p += 1
     end
-    
     tally
   end
 end
@@ -38,3 +21,39 @@ end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 p substrings("below", dictionary)
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+# Kept thinking in a C way here for some reason :>
+# def substrings(original, subs)
+#   subs.reduce(Hash.new) do |tally, sub|
+#     original_a = original.downcase
+#     p = 0
+    
+#     while p < original.length - 1
+#       if sub[0] == original_a[p]
+#         j = 1
+
+#         while j <= sub.length-1
+#           p += 1
+          
+#           if sub[j] != original_a[p]
+#             break
+#           end
+
+#           j += 1
+#         end
+        
+#         if j == sub.length
+#           if tally.dig(sub)
+#             tally[sub] += 1
+#           else
+#             tally[sub] = 1
+#           end
+#         end
+#       end
+      
+#       p += 1
+#     end
+    
+#     tally
+#   end
+# end
